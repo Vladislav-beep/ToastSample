e2fb008a42a9111068e2c9a3a94c6b557565b4f6
VLADISLAV <SIZONOV.95@MAIL.RU>
2023-03-21 20:11:46 +0300

    added notification service

---
+++ ToastSample/NotificationService.swift
@@ -0,0 +1,75 @@
+//
+//  NotificationService.swift
+//  ToastSample
+//
+//  Created by Владислав Сизонов on 21.03.2023.
+//
+
+import Foundation
+import UIKit
+
+final class NotificationService {
+    
+    // MARK: Public
+    
+    static func showToast(message: String, icon: String) {
+        guard let window = UIApplication.shared.windows.first else {
+            return
+        }
+        let toastLbl = UILabel()
+        toastLbl.text = message
+        toastLbl.textAlignment = .center
+        toastLbl.font = UIFont.systemFont(ofSize: 18)
+        toastLbl.textColor = .black
+        toastLbl.numberOfLines = 0
+        toastLbl.translatesAutoresizingMaskIntoConstraints = false
+        
+        let textSize = toastLbl.intrinsicContentSize
+        let toastWidth = UIScreen.main.bounds.width / 9 * 7
+        let labelHeight = (textSize.width / toastWidth) * 50
+        let adjustedHeight = max(labelHeight, 60)
+        
+        let backView = UIView()
+        backView.backgroundColor = .blue
+        backView.layer.cornerRadius = 30
+        backView.layer.shadowRadius = 4
+        backView.layer.shadowOpacity = 0.4
+        backView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        backView.translatesAutoresizingMaskIntoConstraints = false
+        
+        window.addSubview(backView)
+        NSLayoutConstraint.activate([
+            backView.topAnchor.constraint(equalTo: window.safeAreaLayoutGuide.topAnchor, constant: 20),
+            backView.centerXAnchor.constraint(equalTo: window.centerXAnchor),
+            backView.widthAnchor.constraint(equalToConstant: toastWidth),
+            backView.heightAnchor.constraint(equalToConstant: adjustedHeight)
+        ])
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: icon)
+        imageView.layer.cornerRadius = 18
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        backView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: backView.centerYAnchor),
+            imageView.leadingAnchor.constraint(equalTo: backView.leadingAnchor, constant: 12),
+            imageView.heightAnchor.constraint(equalToConstant: 36),
+            imageView.widthAnchor.constraint(equalToConstant: 36)
+        ])
+        
+        backView.addSubview(toastLbl)
+        NSLayoutConstraint.activate([
+            toastLbl.centerYAnchor.constraint(equalTo: backView.centerYAnchor),
+            toastLbl.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 4),
+            toastLbl.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -8),
+        ])
+        
+        UIView.animate(withDuration: 3.0, animations: {
+            backView.alpha = 0
+        }) { (_) in
+            toastLbl.removeFromSuperview()
+        }
+    }
+}
--- ToastSample/ViewController.swift
+++ ToastSample/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ToastViewController: UIViewController {
+    
+    private let toastButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show toast", for: .normal)
+        button.backgroundColor = .systemTeal
+        button.layer.cornerRadius = 12
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
     }
-
-
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        toastButton.addTarget(self, action: #selector(showToast), for: .touchUpInside)
+        
+        view.addSubview(toastButton)
+        NSLayoutConstraint.activate([
+            toastButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            toastButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            toastButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            toastButton.heightAnchor.constraint(equalToConstant: 56)
+        ])
+    }
+    
+    @objc func showToast() {
+        NotificationService.showToast(message: "lol", icon: "berry")
+    }
 }
-
